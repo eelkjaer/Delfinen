e58de6e6960cbb651adbe796559816e844f87999
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-05-06 12:58:56 +0200

    Initial commit

--- src/main/java/Data/MemberMapper.java
+++ src/main/java/Data/MemberMapper.java
@@ -1,4 +1,41 @@
 package Data;
 
+import Model.Member;
+import java.util.ArrayList;
+
 public class MemberMapper {
+    /*
+     * Henter alle medlemmer fra SQL
+     * Opretter Member objekter
+     * Tilføjer dem til en midlertidig ArrayList som bliver retuneret
+     */
+    protected ArrayList<Member> getAllMembers(){
+        ArrayList<Member> tmpMembers = new ArrayList<>();
+
+        return tmpMembers;
+    }
+
+    /*
+     * Indsætter nyt Member objekt i SQL og retunerer dette med korrekt id
+     */
+    protected Member createNewMember(Member member){
+        Member tmpMember = member;
+
+        return tmpMember;
+    }
+
+    /*
+     * Ændre data i SQL på medlemmet.
+     */
+    protected Member editMember(int id){
+
+        return null;
+    }
+
+    /*
+     * Sletter valgt medlem fra SQL
+     */
+    protected boolean deleteMember(int id){
+        return false;
+    }
 }
